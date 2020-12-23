e770b596a3e709ec1a57621af8a782431c9df3f0
vrodedanya <socurahlem@yandex.ru>
2020-12-23 14:08:45 +0300

    Added sql requests file

---
+++ students/y2337/Kolotushkin_Danil/Pr3.1/requests.sql
@@ -0,0 +1,7 @@
+insert into Movie (title, year, director) VALUES ("Interstellar", 2014, "Christopher Nolan");
+insert into Movie (title, director) values("Some title", "Director?");
+update Reviewer set name = "Jhon North" where rID = 3;
+update Ratings set stars = 4;
+delete from Ratings;
+delete from Ratings where rID = 2;
+
