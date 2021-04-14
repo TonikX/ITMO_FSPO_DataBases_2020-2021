57de602bd4c3b2a3a8c5366a1eb441e7c02c4ea7
Art3A <artem02artamonov@gmail.com>
2021-04-14 14:56:43 +0300

    more more queries

--- students/y2337/Artamonov_Artyom/laboratory_works/L5/queries.sql
+++ students/y2337/Artamonov_Artyom/laboratory_works/L5/queries.sql
@@ -7,3 +7,7 @@ select id_pupil, avg(mark) from marks group by id_pupil having avg(mark) < 4.5;
 select * from teacher where exists(select 1 from class where id_teacher = id);
 select * from pupil except select * from pupil where gender = 'female';
 select * from teacher right join class on class.id_teacher = teacher.id;
+select day_of_week, subject.name, teacher.name as teacher_name from timetable right join teacher on timetable.id_teacher = teacher.id right join subject on timetable.id_subject = subject.id;
+select name, mark from pupil right join marks on marks.id_pupil = id;
+select letter, number, subject.name from room right join timetable on timetable.id_room = room.id right join subject on id_subject = subject.id;
+
