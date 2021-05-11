c8a8c3e0892cccfea912ed11817a80be9d6c43f7
Xatsker <55413976+Xatsker@users.noreply.github.com>
2021-05-11 18:32:27 +0300

    Delete students/y2336/Shnek_Dmitriy/PR_3.1 directory

--- students/y2336/Shnek_Dmitriy/PR_3.1/zapros.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM Rating WHERE stars > 3;
-INSERT INTO movie (title, year, direction) VALUES ("NEW MOVIE", 2021, "Anonimus");
-SELECT MIN(stars) AS SmallestStars FROM Rating;
-DELETE FROM Reviewer WHERE rID = (SELECT rID FROM Reviewer ORDER BY rID DESC LIMIT 1); 
