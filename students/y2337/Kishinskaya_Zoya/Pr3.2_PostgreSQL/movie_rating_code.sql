8d1c0638e8f47f66c1131d5261d975d116911e39
zoechcaa <zoakisinskaa1@gmail.com>
2021-09-06 14:13:16 +0300

    ИТМО ФСПО Y2337 Практическая работа 3.2 Кишинская Зоя

---
+++ students/y2337/Kishinskaya_Zoya/Pr3.2_PostgreSQL/movie_rating_code.sql
@@ -0,0 +1,6 @@
+INSERT INTO Movie (mID, title, year) VALUES (227, "Middle-earth", 2000);
+INSERT INTO Reviewer (rID, name) VALUES (300, "Hernado Martinez");
+UPDATE Movie SET year = 2077;
+UPDATE Movie SET director = "PogChamp" WHERE director = "Steven Spielberg";
+DELETE FROM Rating;
+DELETE FROM Movie WHERE director = "James Cameron"; 
